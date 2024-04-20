38d24e313ba1edad1d26428bbf91792b1c8bb3a9
azami.a <azami.a@VDI-DOTNET2-22>
2024-04-20 18:12:29 +0430

    Add project files.

---
+++ ShMa.Domain/ProductCategoryAgg/ProductCategoryDomain.cs
@@ -0,0 +1,40 @@
+﻿using FrameWork.Domain;
+
+namespace ShMa.Domain.ProductCategoryAgg
+{
+    public class ProductCategoryDomain : EntityBase
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+        public string ImageTitle { get; private set; }
+        public string ImageAlt { get; private set; }
+        public string KeyWord { get; private set; }
+        public string MetaDescription { get; private set; }
+        public string Slug { get; private set; }
+
+        public ProductCategoryDomain(string name, string description, string image, string imageTitle, string imageAlt, string keyWord, string metaDescription, string slug)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            ImageTitle = imageTitle;
+            ImageAlt = imageAlt;
+            KeyWord = keyWord;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+
+        public void Edit(string name, string description, string image, string imageTitle, string imageAlt, string keyWord, string metaDescription, string slug)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            ImageTitle = imageTitle;
+            ImageAlt = imageAlt;
+            KeyWord = keyWord;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+    }
+}
